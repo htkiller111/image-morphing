e1a7442334f2055a5693259482ade7d7084076b2
samuelchvez@gmail.com <samuelchvez@gmail.com@88544cd5-0b9c-b7f8-76f4-635a8c702b72>
2011-08-15 16:26:49 +0000

    First slice of warping triangle algorithm working.

--- trunk/src/imagemorphing/ImagePanel.java
+++ trunk/src/imagemorphing/ImagePanel.java
@@ -23,6 +23,8 @@ import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 // Logics
 
@@ -43,15 +45,24 @@ public class ImagePanel extends JPanel implements MouseListener{
     
     private RenderedImage sourceImage;
     
+    private BufferedImage buffer;
+    private BufferedImage sourceBuffer;
+    
 // Debug
 //    private static final int CENTER_FACTOR = 10;
     
     public ImagePanel(String imageFile){
         sourceImage = JAI.create("fileload", imageFile);
+        
         triangulation = new Triangulation(
                 new Vector2D(-1.0, -1.0*M_HEIGHT),
                 new Vector2D(-1.0, M_HEIGHT + 1.0),
                 new Vector2D(2.0*M_WIDTH, M_HEIGHT + 1.0));
+        
+        buffer = new BufferedImage(
+                M_WIDTH, 
+                M_HEIGHT, 
+                BufferedImage.TYPE_INT_ARGB);
         
 // Debug
 //      triangulation = new Triangulation(
@@ -117,13 +128,89 @@ public class ImagePanel extends JPanel implements MouseListener{
     }
     @Override
     public void paint(Graphics g){
-        g.setColor(Color.white);
-        g.fillRect(0, 0, 2*M_WIDTH, 2*M_HEIGHT);
-        ((Graphics2D)g).drawRenderedImage(sourceImage, new AffineTransform());
-        drawTriangulation(g, triangulation);
+        // New buffer
+        buffer = new BufferedImage(
+                M_WIDTH, 
+                M_HEIGHT, 
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics bg = buffer.getGraphics();
+        bg.setColor(Color.white);
+        bg.fillRect(0, 0, 2*M_WIDTH, 2*M_HEIGHT);
+        ((Graphics2D)bg).drawRenderedImage(
+                sourceImage, 
+                new AffineTransform());
+        //drawTriangulation(bg, triangulation);
+        
+        java.util.LinkedList<Edge2D> edges = new java.util.LinkedList<Edge2D>();
+        
+        // source
+        edges.add(new Edge2D(new Vector2D(130, 200), new Vector2D(100, 250)));
+        edges.add(new Edge2D(new Vector2D(100, 250), new Vector2D(200, 300)));
+        edges.add(new Edge2D(new Vector2D(200, 300), new Vector2D(130, 200)));
+        Triangle2D stryangle = new Triangle2D(edges.get(0), edges.get(1), edges.get(2));
+        
+        // destination
+        edges.add(new Edge2D(new Vector2D(400, 200), new Vector2D(400, 300)));
+        edges.add(new Edge2D(new Vector2D(400, 300), new Vector2D(500, 300)));
+        edges.add(new Edge2D(new Vector2D(500, 300), new Vector2D(400, 200)));
+        Triangle2D dtryangle = new Triangle2D(edges.get(3), edges.get(4), edges.get(5));
+        
+        for(Edge2D edge: edges)
+            drawEdge(bg, edge);
+        
+        mapTexture(bg, stryangle, dtryangle);
+        
+        g.drawImage(buffer, 0, 0, this);
+    }
+    private void mapTexture(Graphics g, Triangle2D s, Triangle2D d){
+        
+        g.setColor(Color.green);
+        int[] box = d.minimumSuroundingBox();
+        Vector2D test = new Vector2D(0,0);
+        int i_argb = 0;
+        int[] test_argb = new int[4];
+        int xo, xf;
+        double[] barycentric_d = new double[3];
+        double[] cartesian_s = new double[2];
+        
+        HashMap<Integer, LinkedList<Integer>> levels = 
+                LAT.levels(d);
+        for(Integer level: levels.keySet()){
+            xo = levels.get(level).get(0);
+            xf = levels.get(level).get(1);
+            for(int x = xo; x <= xf; x++){
+                barycentric_d = LAT.getBarycentricCoords(x, level, d);
+                cartesian_s = LAT.restoreBarycentricCoords(
+                        barycentric_d[0],
+                        barycentric_d[1],
+                        barycentric_d[2],
+                        s);
+                i_argb = buffer.getRGB((int)cartesian_s[0], (int)cartesian_s[1]);
+                g.setColor(new Color(i_argb));
+//                test_argb = constructARGB(i_argb);
+//                g.setColor(new Color(
+//                        test_argb[1], 
+//                        test_argb[2], 
+//                        test_argb[3], 
+//                        test_argb[0]));
+                
+                g.drawLine(x, level, x, level);
+            }
+            //g.drawLine(xo, level, xf, level);
+        }
+        
     }
     private void manageNewPoint(Vector2D p){
-        triangulation.addPoint(p);
+        //triangulation.addPoint(p);
+    }
+    private int[] constructARGB(int argb){
+        int[] tor = new int[4];
+        String s_argb = Integer.toBinaryString(argb);
+        tor[0] = Integer.parseInt(s_argb.substring(0, 8), 2);
+        tor[1] = Integer.parseInt(s_argb.substring(8, 16), 2);
+        tor[2] = Integer.parseInt(s_argb.substring(16, 24), 2);
+        tor[3] = Integer.parseInt(s_argb.substring(24, 32), 2);
+        return tor;
     }
     @Override
     public Dimension getPreferredSize() {
@@ -137,7 +224,9 @@ public class ImagePanel extends JPanel implements MouseListener{
     @Override
     public void mouseClicked(MouseEvent me) {}
     @Override
-    public void mousePressed(MouseEvent me) {}
+    public void mousePressed(MouseEvent me) {
+        constructARGB(buffer.getRGB(me.getX(), me.getY()));
+    }
     @Override
     public void mouseEntered(MouseEvent me) {}
     @Override
